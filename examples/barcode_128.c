032baaba469214c3fcc00f3339666de3b869e768
jlsuh <joelleonardosuh@gmail.com>
2026-02-03 19:17:52 -0300

    feat: add support for set A

--- examples/barcode_128.c
+++ examples/barcode_128.c
@@ -27,16 +27,16 @@ int get_height(void)
 }
 
 int dpr = 1;
-void set_dpr(int userDpr)
+void set_dpr(int user_dpr)
 {
-    if (userDpr < 1)
-        userDpr = 1;
-    if (userDpr > 4)
-        userDpr = 4;
-    dpr = userDpr;
+    if (user_dpr < 1)
+        user_dpr = 1;
+    if (user_dpr > 4)
+        user_dpr = 4;
+    dpr = user_dpr;
 }
 
-#define MAX_WIDTH 14000
+#define MAX_WIDTH 13000
 #define MAX_HEIGHT 1200
 
 uint32_t pixels[MAX_WIDTH * MAX_HEIGHT];
@@ -45,13 +45,30 @@ uint32_t *get_pixel_buffer(void)
     return pixels;
 }
 
-#define BASE_MODULE_WIDTH_PX 4
 #define BASE_BAR_HEIGHT_PX 160
+#define BASE_MODULE_WIDTH_PX 4
 #define BASE_VERTICAL_QUIET_ZONE_PX 30
 #define MODULES_PER_SYMBOL 11
 #define START_CHECK_STOP_SYMBOLS 3
+
+#define ANY_SUBSET -1
+#define SUBSET_A 0
 #define SUBSET_B 1
 #define SUBSET_C 2
+
+#define CODE_A 101
+#define CODE_B 100
+#define CODE_C 99
+#define FNC_1 102
+#define FNC_2 97
+#define FNC_3 96
+#define FNC_4 107
+#define SHIFT 98
+#define START_A 103
+#define START_B 104
+#define START_C 105
+#define STOP 106
+
 #define PATTERN_WIDTHS_LEN 107
 
 int symbol_buffer[BUFFER_SIZE * 2];
@@ -74,12 +91,21 @@ const char *PATTERN_WIDTHS[PATTERN_WIDTHS_LEN] = {
     "411311", "113141", "114131", "311141", "411131", "211412", "211214",
     "211232", "233111"};
 
-int kernighan_ritchie_strlen(char s[])
+int kernighan_ritchie_strlen(const char *s)
 {
     int i = 0;
     while (s[i] != '\0')
         ++i;
     return i;
+}
+
+bool kernighan_ritchie_strncmp(const char *s1, const char *s2, int n)
+{
+    for (int i = 0; i < n; i++) {
+        if (s1[i] != s2[i] || s1[i] == '\0')
+            return false;
+    }
+    return true;
 }
 
 bool is_digit(char c)
@@ -114,6 +140,49 @@ static int draw_barcode(Dsgl_Canvas *c, int x, int y, const char *pattern_width,
     return curr_x - x;
 }
 
+typedef struct {
+    const char *key;
+    int len;
+    int value;
+    int dest_subset;
+} Keyword;
+
+#define KEYWORDS_LEN 40
+Keyword KEYWORDS[KEYWORDS_LEN] = {
+    {"NUL", 3, 64, SUBSET_A},         {"SOH", 3, 65, SUBSET_A},
+    {"STX", 3, 66, SUBSET_A},         {"ETX", 3, 67, SUBSET_A},
+    {"EOT", 3, 68, SUBSET_A},         {"ENQ", 3, 69, SUBSET_A},
+    {"ACK", 3, 70, SUBSET_A},         {"BEL", 3, 71, SUBSET_A},
+    {"BS", 2, 72, SUBSET_A},          {"HT", 2, 73, SUBSET_A},
+    {"LF", 2, 74, SUBSET_A},          {"VT", 2, 75, SUBSET_A},
+    {"FF", 2, 76, SUBSET_A},          {"CR", 2, 77, SUBSET_A},
+    {"SO", 2, 78, SUBSET_A},          {"SI", 2, 79, SUBSET_A},
+    {"DLE", 3, 80, SUBSET_A},         {"DC1", 3, 81, SUBSET_A},
+    {"DC2", 3, 82, SUBSET_A},         {"DC3", 3, 83, SUBSET_A},
+    {"DC4", 3, 84, SUBSET_A},         {"NAK", 3, 85, SUBSET_A},
+    {"SYN", 3, 86, SUBSET_A},         {"ETB", 3, 87, SUBSET_A},
+    {"CAN", 3, 88, SUBSET_A},         {"EM", 2, 89, SUBSET_A},
+    {"SUB", 3, 90, SUBSET_A},         {"ESC", 3, 91, SUBSET_A},
+    {"FS", 2, 92, SUBSET_A},          {"GS", 2, 93, SUBSET_A},
+    {"RS", 2, 94, SUBSET_A},          {"US", 2, 95, SUBSET_A},
+    {"DEL", 3, 95, SUBSET_B},         {"FNC1", 4, FNC_1, ANY_SUBSET},
+    {"FNC2", 4, FNC_2, ANY_SUBSET},   {"FNC3", 4, FNC_3, ANY_SUBSET},
+    {"FNC4", 4, FNC_4, ANY_SUBSET},   {"CODEA", 5, CODE_A, ANY_SUBSET},
+    {"CODEB", 5, CODE_B, ANY_SUBSET}, {"CODEC", 5, CODE_C, ANY_SUBSET}};
+
+int match_keyword(const char *data_buffer, int idx)
+{
+    if (data_buffer[idx] != '^')
+        return -1;
+    for (int k = 0; k < KEYWORDS_LEN; k++) {
+        if (kernighan_ritchie_strncmp(&data_buffer[idx + 1], KEYWORDS[k].key,
+                                      KEYWORDS[k].len)) {
+            return k;
+        }
+    }
+    return -1;
+}
+
 void render(void)
 {
     int module_width_px = BASE_MODULE_WIDTH_PX * dpr;
@@ -126,35 +195,127 @@ void render(void)
     int curr_subset = SUBSET_B;
     if (check_digits(data_buffer, 0, 4, data_len)) {
         curr_subset = SUBSET_C;
-        symbol_buffer[next_symbol_idx++] = 105;
+        symbol_buffer[next_symbol_idx++] = START_C;
     } else {
-        curr_subset = SUBSET_B;
-        symbol_buffer[next_symbol_idx++] = 104;
+        int keyword_idx = match_keyword(data_buffer, 0);
+        if (keyword_idx != -1 &&
+            KEYWORDS[keyword_idx].dest_subset == SUBSET_A) {
+            curr_subset = SUBSET_A;
+            symbol_buffer[next_symbol_idx++] = START_A;
+        } else {
+            curr_subset = SUBSET_B;
+            symbol_buffer[next_symbol_idx++] = START_B;
+        }
     }
     while (next_input_idx < data_len) {
-        if (curr_subset == SUBSET_B) {
-            if (check_digits(data_buffer, next_input_idx, 4, data_len)) {
-                symbol_buffer[next_symbol_idx++] = 99;
-                curr_subset = SUBSET_C;
-            } else {
-                char c = data_buffer[next_input_idx];
-                if (c >= 32 && c <= 127) {
-                    symbol_buffer[next_symbol_idx++] = c - 32;
-                } else {
-                    symbol_buffer[next_symbol_idx++] = 0;
+        int keyword_idx = match_keyword(data_buffer, next_input_idx);
+        if (keyword_idx != -1) {
+            Keyword k = KEYWORDS[keyword_idx];
+            if (k.dest_subset != ANY_SUBSET && k.dest_subset != curr_subset) {
+                if (k.dest_subset == SUBSET_A) {
+                    symbol_buffer[next_symbol_idx++] = CODE_A;
+                    curr_subset = SUBSET_A;
+                } else if (k.dest_subset == SUBSET_B) {
+                    symbol_buffer[next_symbol_idx++] = CODE_B;
+                    curr_subset = SUBSET_B;
                 }
-                next_input_idx++;
             }
-        } else if (curr_subset == SUBSET_C) {
+            if (k.value == CODE_A)
+                curr_subset = SUBSET_A;
+            if (k.value == CODE_B)
+                curr_subset = SUBSET_B;
+            if (k.value == CODE_C)
+                curr_subset = SUBSET_C;
+            if (k.value == FNC_4) {
+                if (curr_subset == SUBSET_A) {
+                    symbol_buffer[next_symbol_idx++] = 101;
+                } else if (curr_subset == SUBSET_B) {
+                    symbol_buffer[next_symbol_idx++] = 100;
+                } else {
+                    symbol_buffer[next_symbol_idx++] = CODE_B;
+                    curr_subset = SUBSET_B;
+                    symbol_buffer[next_symbol_idx++] = 100;
+                }
+            } else {
+                symbol_buffer[next_symbol_idx++] = k.value;
+            }
+            next_input_idx += (1 + k.len);
+            continue;
+        }
+        if (curr_subset == SUBSET_C) {
             if (check_digits(data_buffer, next_input_idx, 2, data_len)) {
                 int d1 = data_buffer[next_input_idx] - '0';
                 int d2 = data_buffer[next_input_idx + 1] - '0';
-                int value = (d1 * 10) + d2;
-                symbol_buffer[next_symbol_idx++] = value;
+                symbol_buffer[next_symbol_idx++] = (d1 * 10) + d2;
                 next_input_idx += 2;
             } else {
-                symbol_buffer[next_symbol_idx++] = 100;
+                symbol_buffer[next_symbol_idx++] = CODE_B;
                 curr_subset = SUBSET_B;
+            }
+        } else {
+            bool is_escape_ahead =
+                (match_keyword(data_buffer, next_input_idx) != -1);
+            if (!is_escape_ahead &&
+                check_digits(data_buffer, next_input_idx, 4, data_len)) {
+                symbol_buffer[next_symbol_idx++] = CODE_C;
+                curr_subset = SUBSET_C;
+                continue;
+            }
+            char c = data_buffer[next_input_idx];
+            if (curr_subset == SUBSET_B) {
+                if (c >= 0 && c <= 31) {
+                    bool next_is_ctrl = false;
+                    if (next_input_idx + 1 < data_len) {
+                        char next_c = data_buffer[next_input_idx + 1];
+                        if (next_c >= 0 && next_c <= 31)
+                            next_is_ctrl = true;
+                    }
+                    if (next_is_ctrl) {
+                        symbol_buffer[next_symbol_idx++] = CODE_A;
+                        curr_subset = SUBSET_A;
+                        symbol_buffer[next_symbol_idx++] = c + 64;
+                    } else {
+                        symbol_buffer[next_symbol_idx++] = SHIFT;
+                        symbol_buffer[next_symbol_idx++] = c + 64;
+                    }
+                    next_input_idx++;
+                } else if (c >= 32 && c <= 127) {
+                    symbol_buffer[next_symbol_idx++] = c - 32;
+                    next_input_idx++;
+                } else {
+                    symbol_buffer[next_symbol_idx++] = 0;
+                    next_input_idx++;
+                }
+            } else if (curr_subset == SUBSET_A) {
+                if (c >= 97 && c <= 122) {
+                    bool next_is_lower = false;
+                    if (next_input_idx + 1 < data_len) {
+                        char next_c = data_buffer[next_input_idx + 1];
+                        if (next_c >= 97 && next_c <= 122)
+                            next_is_lower = true;
+                    }
+                    if (next_is_lower) {
+                        symbol_buffer[next_symbol_idx++] = CODE_B;
+                        curr_subset = SUBSET_B;
+                        symbol_buffer[next_symbol_idx++] = c - 32;
+                    } else {
+                        symbol_buffer[next_symbol_idx++] = SHIFT;
+                        symbol_buffer[next_symbol_idx++] = c - 32;
+                    }
+                    next_input_idx++;
+                } else if (c >= 32 && c <= 95) {
+                    symbol_buffer[next_symbol_idx++] = c - 32;
+                    next_input_idx++;
+                } else if (c >= 0 && c <= 31) {
+                    symbol_buffer[next_symbol_idx++] = c + 64;
+                    next_input_idx++;
+                } else if (c >= 96 && c <= 127) {
+                    symbol_buffer[next_symbol_idx++] = CODE_B;
+                    curr_subset = SUBSET_B;
+                    continue;
+                } else {
+                    next_input_idx++;
+                }
             }
         }
     }
@@ -163,10 +324,10 @@ void render(void)
         dividend += (symbol_buffer[i] * i);
     }
     symbol_buffer[next_symbol_idx++] = dividend % 103;
-    symbol_buffer[next_symbol_idx++] = 106;
-    int data_symbols = next_symbol_idx - START_CHECK_STOP_SYMBOLS;
-    int total_modules = 57 + (data_symbols * MODULES_PER_SYMBOL);
-    canvas_width = total_modules * module_width_px;
+    symbol_buffer[next_symbol_idx++] = STOP;
+    int total_modules = (next_symbol_idx * MODULES_PER_SYMBOL) + 2;
+    canvas_width =
+        total_modules * module_width_px + (2 * horizontal_quiet_zone_px);
     canvas_height = bar_height_px + (2 * vertical_quiet_zone_px);
     Dsgl_Canvas c = dsgl_create_canvas(pixels, canvas_width, canvas_height);
     dsgl_fill_rect(&c, 0, 0, canvas_width, canvas_height, 0xFFFFFFFF);
